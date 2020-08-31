326747daff485a2e6028b392a325b57cefab9004
Herman Zubenko <herman.zubenko@gmail.com>
2020-08-31 21:40:05 +0300

    Added supertest:

--- tests/example/example.manga.test.js
+++ tests/example/example.manga.test.js
@@ -5,8 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const assert_1 = __importDefault(require("assert"));
 const request_1 = require("../../core/request");
+const supertest_1 = __importDefault(require("supertest"));
 const example_testData_1 = require("./example.testData");
 describe('Example query for Manga ', async () => {
+    it('supertest test!', async () => {
+        const test = await supertest_1.default('https://graphql.anilist.co')
+            .post('')
+            .send({ query: example_testData_1.queryManga })
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .expect(200);
+        console.log(test.body.data.Media);
+    });
     it('should assert status code', async () => {
         const statusCode = await request_1.getStatusCode(example_testData_1.queryManga);
         assert_1.default.equal(statusCode, 200);
--- tests/example/example.manga.test.ts
+++ tests/example/example.manga.test.ts
@@ -1,9 +1,22 @@
 import assert from 'assert';
 import {getStatusCode, getResponseBody} from '../../core/request';
 
+import request from 'supertest';
+
 import {queryManga} from './example.testData';
 
 describe('Example query for Manga ', async () => {
+	it('supertest test!', async () => {
+		const test = await request('https://graphql.anilist.co')
+			.post('')
+			.send({query: queryManga})
+			.set('Content-Type', 'application/json')
+			.set('Accept', 'application/json')
+			.expect(200)
+
+		console.log(test.body.data.Media);
+	})
+
 	it('should assert status code', async () => {
 		const statusCode = await getStatusCode(queryManga);
 
